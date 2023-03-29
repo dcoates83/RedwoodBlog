c5cd73903574df982513e8afcffa88dab0dd03d6
Devon <devon.coates@totaleto.com>
2023-03-29 15:22:56 -0600

    changes

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -15,6 +15,7 @@ import PostsLayout from './layouts/PostsLayout/PostsLayout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
       <Set wrap={PostsLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
         <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
         <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -1,10 +1,10 @@
-import type { FindPosts } from 'types/graphql'
-
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
+import Article from '../Article/Article'
+
 export const QUERY = gql`
-  query FindPosts {
-    posts {
+  query ArticlesQuery {
+    articles: posts {
       id
       title
       body
@@ -21,11 +21,12 @@ export const Failure = ({ error }: CellFailureProps) => (
   <div>Error loading posts: {error.message}</div>
 )
 
-export const Success = ({ posts }: CellSuccessProps<FindPosts>) => {
-  return posts.map((post) => (
-    <article key={post.id}>
-      <h2>{post.title}</h2>
-      <div>{post.body}</div>
-    </article>
-  ))
+export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
+  return (
+    <>
+      {articles.map((article) => (
+        <Article key={article.id} article={article} />
+      ))}
+    </>
+  )
 }
